d76d4097ca0cceef703a30525ad293fa611ec70c
unknown <you@example.com>
2022-10-25 12:13:57 -0500

    made easy going function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("first commit")
+//console.log("first commit")
+const logNumbers = () => {
+    for(let i = 1; i <= 20; i++){
+        console.log(i)
+    }
+    
+}
+logNumbers();
+logNumbers();
+logNumbers();
+logNumbers();
+logNumbers();
