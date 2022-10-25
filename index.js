92a70fc6a277df84884b6ed16af873412d5cb56b
unknown <you@example.com>
2022-10-25 12:36:38 -0500

    add a new function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //console.log("first commit")
+//Esay going
+
+//function 1
 const logNumbers = () => {
     for(let i = 1; i <= 20; i++){
         console.log(i)
@@ -10,3 +13,12 @@ logNumbers();
 logNumbers();
 logNumbers();
 logNumbers();
+
+// commit the changes to github
+const getEven = () => {
+    for(i=0; i<=200; i++){
+        if(i % 2 == 0 && i !==0){
+            console.log(i);
+        }
+    }
+}
