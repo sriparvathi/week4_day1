c106ca160584905614975ebad51fb696decd64a1
unknown <you@example.com>
2022-10-25 12:44:38 -0500

    new function added

--- index.js
+++ index.js
@@ -2,23 +2,47 @@
 //Esay going
 
 //function 1
-const logNumbers = () => {
-    for(let i = 1; i <= 20; i++){
-        console.log(i)
-    }
+// const logNumbers = () => {
+//     for(let i = 1; i <= 20; i++){
+//         console.log(i)
+//     }
     
-}
-logNumbers();
-logNumbers();
-logNumbers();
-logNumbers();
-logNumbers();
+// }
+// logNumbers();
+// logNumbers();
+// logNumbers();
+// logNumbers();
+// logNumbers();
 
 // commit the changes to github
-const getEven = () => {
-    for(i=0; i<=200; i++){
-        if(i % 2 == 0 && i !==0){
-            console.log(i);
-        }
-    }
-}
+// const getEven = () => {
+//     for(i=0; i<=200; i++){
+//         if(i % 2 == 0 && i !==0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+//fizz Buzz
+    // const getMyNumbers = () =>{
+    //     let num;
+    //         for(let i=1; i<101; i++){
+    //             num=i;
+            
+    //         if(num%3===0 && num%5===0){
+    //             console.log(`FizzBuzz`);
+    //         }
+    //         else if(num%3===0){
+    //             console.log(`Fizz`);
+    //         }
+    //         else if(num%5===0){
+    //             console.log(`Buzz`);
+    //         }
+    //         else {
+    //             console.log(num);
+    //         }
+    //     }
+    // }
+    // getMyNumbers();
+
+
